92c5da05ea55970c028061740be29a2ba749d185
oladapooloyede <30477121+oladapooloyede@users.noreply.github.com>
2025-03-22 20:07:07 -0600

    Update setup-haproxy.sh

--- scripts/setup-haproxy.sh
+++ scripts/setup-haproxy.sh
@@ -5,11 +5,17 @@ IP_MASTER0=192.168.0.90
 IP_MASTER1=192.168.0.91
 IP_MASTER2=192.168.0.92
 
+IP_WORKER0=192.168.0.90
+IP_WORKER1=192.168.0.91
+IP_WORKER2=192.168.0.92
+
 cp extras/haproxy.cfg.ORIG extras/haproxy.cfg
 
 sed -Ezi "s/IP_BOOTSTRAP/$IP_BOOTSTRAP/g" extras/haproxy.cfg
 sed -Ezi "s/IP_MASTER0/$IP_MASTER0/g" extras/haproxy.cfg
 sed -Ezi "s/IP_MASTER1/$IP_MASTER1/g" extras/haproxy.cfg
 sed -Ezi "s/IP_MASTER2/$IP_MASTER2/g" extras/haproxy.cfg
-
+sed -Ezi "s/IP_MASTER0/$IP_WORKER0/g" extras/haproxy.cfg
+sed -Ezi "s/IP_MASTER1/$IP_WORKER1/g" extras/haproxy.cfg
+sed -Ezi "s/IP_MASTER2/$IP_WORKER2/g" extras/haproxy.cfg
 ./scripts/create-haproxy.sh
